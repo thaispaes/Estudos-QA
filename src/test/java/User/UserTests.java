05e79de0ee3068a9d517640e878768173abce5fc
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-01-20 16:58:50 -0300

    Feat: criando primeiro teste

--- src/test/java/User/UserTests.java
+++ src/test/java/User/UserTests.java
@@ -1,14 +1,17 @@
 package User;
 
-
 import Entities.User;
+import org.junit.jupiter.api.*;
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.config.LogConfig.logConfig;
+import static org.hamcrest.Matchers.*;
+
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserTests {
@@ -19,12 +22,13 @@ public class UserTests {
     public static RequestSpecification request;
 
     @BeforeAll
-    public void setup() {
+    public static void setup() {
         RestAssured.baseURI = "https://petstore.swagger.io/v2/";
 
         faker = new Faker();
 
-        user = new User(faker.name().username(),
+        user = new User(
+                faker.name().username(),
                 faker.name().firstName(),
                 faker.name().lastName(),
                 faker.internet().safeEmailAddress(),
@@ -39,4 +43,22 @@ public class UserTests {
                 .contentType(ContentType.JSON);
 
     }
+
+    @Test
+    public void CreateNewUser_WithValidData_ReturnOK(){
+
+        request
+                .body(user)
+                .when()
+                .post("/user")
+                .then()
+                .assertThat().statusCode(200).and()
+                .body("code", equalTo(200))
+                .body("type", equalTo("unknown"))
+                .body("message", isA(String.class))
+                .body("size()", equalTo(3));
+
+    }
+
+
 }
