0b80905b9e763abe79519956db38aacd802757ae
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-01-20 16:33:52 -0300

    Feat: estruturando nosso teste

--- src/test/java/User/UserTests.java
+++ src/test/java/User/UserTests.java
@@ -1,17 +1,42 @@
 package User;
 
 
+import Entities.User;
+import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.given;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserTests {
 
+    private static User user;
+
+    public static Faker faker;
+    public static RequestSpecification request;
+
     @BeforeAll
     public void setup() {
         RestAssured.baseURI = "https://petstore.swagger.io/v2/";
+
+        faker = new Faker();
+
+        user = new User(faker.name().username(),
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.internet().safeEmailAddress(),
+                faker.internet().password(8, 10),
+                faker.phoneNumber().toString());
+    }
+
+    @BeforeEach
+    void setRequest() {
+        request = given()
+                .header("api-key", "special-key")
+                .contentType(ContentType.JSON);
 
     }
 }
